c7d78bac52890d79ec7eeedb8b72100200db60ec
bogdax <bogdax@BogdarK-HP.>
2025-11-22 18:24:06 +0200

    using puppet

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Using puppet to make changes to our congiguration file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
